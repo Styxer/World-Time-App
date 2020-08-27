b487ac516ebef48c78594f7e7b48a7814acdf122
ofir <gennin@gmail.com>
2020-08-27 13:08:17 +0300

    basic read from time API

--- world_time/lib/services/world_time.dart
+++ world_time/lib/services/world_time.dart
@@ -12,17 +12,22 @@ class WorldTime {
 
   Future<void> getData() async {
 
-    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
-    Map data = jsonDecode(response.body);
+    try {
+      Response response = await get('http://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
 
-    //get properties from data
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
+      //get properties from data
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1,3);
 
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
 
-    time = now.toString();
+      time = now.toString();
+    } catch (e) {
+      print(e);
+      time = 'could not get time data';
+    }
 
   }
 }
